6a7fb626961afc231eaf0b65e0a965cc3dee9291
trungthanhly <119275248+jaesungl33@users.noreply.github.com>
2023-09-09 14:17:12 +0700

    Sung
    hi

--- PortManagement/src/Main.java
+++ PortManagement/src/Main.java
@@ -2,6 +2,9 @@
 // then press Enter. You can now see whitespace characters in your code.
 public class Main {
     public static void main(String[] args) {
-        System.out.printf("Hello World!!!");
+
+        System.out.println("Hello World!!!");
+        System.out.println("Hello World!!!");
     }
+
 }
